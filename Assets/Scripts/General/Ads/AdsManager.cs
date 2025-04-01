9f91d08958b3e7237ad2afc0c9e86a12947b1fbf
自宅デスクトッ\daiki <daiki25760920@gmail.com>
2025-04-02 02:14:59 +0900

    広告ID修正

--- Assets/Scripts/General/Ads/AdsManager.cs
+++ Assets/Scripts/General/Ads/AdsManager.cs
@@ -9,7 +9,7 @@ public static class AdsManager
     private static BannerView _bannerView;
     private const string _testBannerUnitId = "ca-app-pub-3940256099942544/2934735716";
     private const string _testInterstitialUnitId = "ca-app-pub-3940256099942544/4411468910";
-    private const string _testRewardUnitId = "ca-app-pub-3940256099942544/2934735716";
+    private const string _testRewardUnitId = "ca-app-pub-3940256099942544/1712485313";
     private const int _interstitialInterval = 3; // インタースティシャル広告表示の間隔
     private const int _rewardInterval = 1; // リワード広告表示の間隔
 
