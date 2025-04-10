cd00297ce9055a5b1bee82ecaf143e69960eb0a4
自宅デスクトッ\daiki <daiki25760920@gmail.com>
2025-04-11 01:33:36 +0900

    音ズレログ修正

--- Assets/Scripts/Battle/BattleView.cs
+++ Assets/Scripts/Battle/BattleView.cs
@@ -91,6 +91,7 @@ public class BattleView : MonoBehaviour
     private LevelInfo _levelInfo;
     // これが大きいと全体的に叩くのが遅い
     private float _beatDiffTime;
+    private float[] _beatDiffTimeList = new float[10];
 
     private Vector3 _startToTargetVectorLeft => _leftTargetPoint.localPosition - _leftStartTransform.localPosition;
     private Vector3 _startToTargetVectorRight => _rightTargetPoint.localPosition - _rightStartTransform.localPosition;
@@ -353,6 +354,7 @@ public class BattleView : MonoBehaviour
         _rightTargetImage.color = new Color(1, 1, 1, 0.7f);
         _currentState = BattleState.StartBattle;
         _beatDiffTime = 0f;
+        _beatDiffTimeList = new float[10];
         // BallTimeOffset分遅れてBGMスタート
         _startBgmTime = Time.time + _ballTimeOffset;
         _bgmStartCts = new CancellationTokenSource();
@@ -541,6 +543,7 @@ public class BattleView : MonoBehaviour
     public async UniTask StartResultAsync(ClearState clearState, ClearState highScoreClearState, float criticalMultiple, float hitMultiple, float missMultiple)
     {
         DebugPanel.instance.AddLog("BeatDiffTime " + _beatDiffTime);
+        DebugPanel.instance.AddLog("BDTs " + _beatDiffTimeList[0].RoundDown(5) + " " + _beatDiffTimeList[1].RoundDown(5) + " " + _beatDiffTimeList[2].RoundDown(5) + " " + _beatDiffTimeList[3].RoundDown(5) + " " + _beatDiffTimeList[4].RoundDown(5) + " " + _beatDiffTimeList[5].RoundDown(5) + " " + _beatDiffTimeList[6].RoundDown(5) + " " + _beatDiffTimeList[7].RoundDown(5) + " " + _beatDiffTimeList[8].RoundDown(5) + " " + _beatDiffTimeList[9].RoundDown(5));
         await UniTask.WaitForSeconds(_beforeReultWaitTime, cancellationToken: _cts.Token);
         _resultView.SetScore(clearState, highScoreClearState, criticalMultiple, hitMultiple, missMultiple);
         _resultView.gameObject.SetActive(true);
@@ -599,7 +602,9 @@ public class BattleView : MonoBehaviour
             SEManager.instance.PlaySe(SeName.Beat);
         }
         _lastBeatTime = ball.CriticalTime;
-        _beatDiffTime = _currentTime - ball.CriticalTime;
+        float diffTime = _currentTime - ball.CriticalTime;
+        _beatDiffTime += diffTime;
+        _beatDiffTimeList[(int)_currentTime / 15] += diffTime;
         CreateLetter(ball.IsLeft, ball.JudgeBall(_currentTime));
         _currentScore.CountUp(ball.JudgeBall(_currentTime));
         UpdateScoreText(_currentScore);
