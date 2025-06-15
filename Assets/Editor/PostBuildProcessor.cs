14258ceda0c3d7fdb3b6c1088acd0604d749753f
自宅デスクトッ\daiki <daiki25760920@gmail.com>
2025-06-15 18:37:03 +0900

    邪魔なとこコメントアウト

--- Assets/Editor/PostBuildProcessor.cs
+++ Assets/Editor/PostBuildProcessor.cs
@@ -11,41 +11,41 @@ public class PostBuildProcessor
     [PostProcessBuild]
     public static void OnPostProcessBuild(BuildTarget buildTarget, string pathToBuiltProject)
     {
-        if (buildTarget != BuildTarget.iOS) return;
+//         if (buildTarget != BuildTarget.iOS) return;
 
-        // project.pbxproj のパス
-        var projPath = Path.Combine(pathToBuiltProject, "Unity-iPhone.xcodeproj/project.pbxproj");
+//         // project.pbxproj のパス
+//         var projPath = Path.Combine(pathToBuiltProject, "Unity-iPhone.xcodeproj/project.pbxproj");
 
-        PBXProject proj = new PBXProject();
-        proj.ReadFromFile(projPath);
-        Debug.Log("projPath " + projPath);
+//         PBXProject proj = new PBXProject();
+//         proj.ReadFromFile(projPath);
+//         Debug.Log("projPath " + projPath);
 
-#if UNITY_2019_3_OR_NEWER
-        string targetGuid = proj.GetUnityFrameworkTargetGuid();
-#else
-        string targetGuid = proj.TargetGuidByName("UnityFramework");
-#endif
+// #if UNITY_2019_3_OR_NEWER
+//         string targetGuid = proj.GetUnityFrameworkTargetGuid();
+// #else
+//         string targetGuid = proj.TargetGuidByName("UnityFramework");
+// #endif
 
-        Debug.Log("targetGuid " + targetGuid);
-        // フラグを付けたいソースファイル
-        string fileName = "MusicLibraryMediaPicker.mm";
-        string filePath = Path.Combine("Libraries", "MusicPlayerMediaPicker", "Plugins", "iOS", fileName);
+//         Debug.Log("targetGuid " + targetGuid);
+//         // フラグを付けたいソースファイル
+//         string fileName = "MusicLibraryMediaPicker.mm";
+//         string filePath = Path.Combine("Libraries", "MusicPlayerMediaPicker", "Plugins", "iOS", fileName);
 
-        Debug.LogWarning("configNamesCount" + proj.BuildConfigNames().Count());
-        foreach (string configName in proj.BuildConfigNames())
-        {
-            Debug.LogWarning("configName" + configName);
-            string configGuid = proj.BuildConfigByName(targetGuid, configName);
-            if (string.IsNullOrEmpty(configGuid))
-            {
-                Debug.LogWarning($"Config '{configName}' が見つかりませんでした。");
-                continue;
-            }
+//         Debug.LogWarning("configNamesCount" + proj.BuildConfigNames().Count());
+//         foreach (string configName in proj.BuildConfigNames())
+//         {
+//             Debug.LogWarning("configName" + configName);
+//             string configGuid = proj.BuildConfigByName(targetGuid, configName);
+//             if (string.IsNullOrEmpty(configGuid))
+//             {
+//                 Debug.LogWarning($"Config '{configName}' が見つかりませんでした。");
+//                 continue;
+//             }
 
-            proj.AddBuildPropertyForConfig(configGuid, "OTHER_CFLAGS", "-fno-objc-arc");
-        }
+//             proj.AddBuildPropertyForConfig(configGuid, "OTHER_CFLAGS", "-fno-objc-arc");
+//         }
 
-        // 保存
-        proj.WriteToFile(projPath);
+//         // 保存
+//         proj.WriteToFile(projPath);
     }
 }
