f22e7add9b571ecf3c2959f92baa9d9e1ea7e928
自宅デスクトッ\daiki <daiki25760920@gmail.com>
2025-07-17 01:00:59 +0900

    Auto commit

--- Assets/MusicPlayerMediaPicker/Plugins/iOS/MusicLibraryMediaPicker.mm
+++ Assets/MusicPlayerMediaPicker/Plugins/iOS/MusicLibraryMediaPicker.mm
@@ -162,7 +162,7 @@ extern "C" {
      * 指定した曲をエクスポートする
      **************************************/
     void exportItemFromId(NSString* songId) {
-        
+        return;
         /// 曲情報を取得する処理
         MPMediaQuery* songQuery = [MPMediaQuery songsQuery];
         
@@ -179,7 +179,6 @@ extern "C" {
             return;
         }
         UnitySendMessage("MediaController", "OnNativeLog", [songId UTF8String]);
-        return;
         // 安全に unsigned long long に変換
         NSScanner *scanner = [NSScanner scannerWithString:songId];
         unsigned long long targetValue = 0;
