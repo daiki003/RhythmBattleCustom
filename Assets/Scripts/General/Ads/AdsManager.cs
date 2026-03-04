d646c7cda22e8799e07e97f11fefb7781905ba7c
自宅デスクトッ\daiki <daiki25760920@gmail.com>
2026-03-05 03:30:24 +0900

    Auto commit

--- Assets/Scripts/General/Ads/AdsManager.cs
+++ Assets/Scripts/General/Ads/AdsManager.cs
@@ -10,9 +10,7 @@ public static class AdsManager
     private const string _testBannerUnitId = "ca-app-pub-3940256099942544/2934735716";
     private const string _testInterstitialUnitId = "ca-app-pub-3940256099942544/4411468910";
     private const string _testRewardUnitId = "ca-app-pub-3940256099942544/1712485313";
-    private const string _bannerUnitId = "ca-app-pub-3940256099942544/2934735716";
-    private const string _interstitialUnitId = "ca-app-pub-3940256099942544/4411468910";
-    private const string _rewardUnitId = "ca-app-pub-3940256099942544/1712485313";
+    private const string _rewardUnitId = "ca-app-pub-4957358157988887/5349649426";
     private const int _interstitialInterval = 3; // インタースティシャル広告表示の間隔
     private const int _rewardInterval = 1; // リワード広告表示の間隔
 
@@ -71,7 +69,7 @@ public static class AdsManager
     {
         bool finishAds = false;
         bool rewardEarned = false;
-        RewardedAd.Load(_testRewardUnitId, new AdRequest(), (ad, error) =>
+        RewardedAd.Load(_rewardUnitId, new AdRequest(), (ad, error) =>
         {
             if (error != null)
             {
